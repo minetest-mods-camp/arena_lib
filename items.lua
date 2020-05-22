53eec8775497d8fbfd1fd327c8e0df7079e39e00
Zughy <4279489-marco_a@users.noreply.gitlab.com>
2020-05-22 12:40:28 +0200

    Immunity item not droppable nor placeable

--- items.lua
+++ items.lua
@@ -7,6 +7,8 @@ minetest.register_tool("arena_lib:immunity", {
   description = S("You're immune!"),
   inventory_image = "arenalib_immunity.png",
   groups = {not_in_creative_inventory = 1, oddly_breakable_by_hand = "2"},
+  on_place = function() end,
+  on_drop = function() end
 
 })
 
