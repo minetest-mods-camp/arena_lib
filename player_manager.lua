6f6edb438fd6485035a8797c584dbfc1f6d21935
Marco <4279489-marco_a@users.noreply.gitlab.com>
2020-06-09 22:38:28 +0200

    Players in arenas can't see messages of players not in arenas and vice versa

--- player_manager.lua
+++ player_manager.lua
@@ -87,3 +87,22 @@ minetest.register_on_respawnplayer(function(player)
     return true
 
   end)
+
+
+
+minetest.register_on_chat_message(function(p_name, message)
+
+  if arena_lib.is_player_in_arena(p_name) then
+    arena_lib.send_message_players_in_arena(arena_lib.get_arena_by_player(p_name), minetest.format_chat_message(p_name, message))
+    return true
+  else
+    for _, pl_stats in pairs(minetest.get_connected_players()) do
+      local pl_name = pl_stats:get_player_name()
+      if not arena_lib.is_player_in_arena(pl_name) then
+        minetest.chat_send_player(pl_name, minetest.format_chat_message(p_name, message))
+      end
+    end
+  end
+
+  return true
+end)
