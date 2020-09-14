ce93e9422f9ae4bdb8256f96687c2d6424709c2b
Zughy <4279489-marco_a@users.noreply.gitlab.com>
2020-09-14 23:12:39 +0200

    Signs can't be broken by anyone anymore

--- signs.lua
+++ signs.lua
@@ -10,6 +10,8 @@ local function arena_display_format(arena, msg) end
 
 minetest.override_item("default:sign_wall", {
 
+    on_dig = function() end,
+
     on_punch = function(pos, node, puncher, pointed_thing)
 
       local arenaID = minetest.get_meta(pos):get_int("arenaID")
