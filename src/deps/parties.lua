9775a4598d440b8a5f940b19f031cb462ce2ab7e
Zughy <4279489-marco_a@users.noreply.gitlab.com>
2023-01-30 23:36:13 +0100

    Comments

--- src/deps/parties.lua
+++ src/deps/parties.lua
@@ -7,13 +7,12 @@ local S = minetest.get_translator("arena_lib")
 
 
 parties.register_on_pre_party_invite(function(sender, p_name)
-
-  -- se il party leader è in coda
+  -- se il capogruppo è in coda
   if arena_lib.is_player_in_queue(sender) then
     minetest.chat_send_player(sender, minetest.colorize("#e6482e", S("[!] You can't perform this action while in queue!")))
     return false end
 
-  -- se il party leader è in gioco
+  -- se il capogruppo è in gioco
   if arena_lib.is_player_in_arena(sender) then
     minetest.chat_send_player(sender, minetest.colorize("#e6482e", S("[!] You can't perform this action while in game!")))
     return false end
@@ -24,13 +23,12 @@ end)
 
 
 parties.register_on_pre_party_join(function(party_leader, p_name)
-
-  -- se il party leader è in coda
+  -- se il capogruppo è in coda
   if arena_lib.is_player_in_queue(party_leader) then
     minetest.chat_send_player(p_name, minetest.colorize("#e6482e", S("[!] The party leader must not be in queue to perform this action!")))
     return false end
 
-  -- se il party leader è in gioco
+  -- se il capogruppo è in gioco
   if arena_lib.is_player_in_arena(party_leader) then
     minetest.chat_send_player(p_name, minetest.colorize("#e6482e", S("[!] The party leader must not be in game to perform this action!")))
     return false end
